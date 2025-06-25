c4effae2d0e94b25979c661301cb58085fe49c40
lxnneak <lienlinneakarlsson@gmail.com>
2025-06-25 23:21:19 +0200

    Add: logout feature in start.java

--- src/main/java/atm/simulator/Start.java
+++ src/main/java/atm/simulator/Start.java
@@ -10,49 +10,59 @@ public class Start {
     Bank bank = new Bank();
 
     boolean running = true;
+    boolean loggedIn = false;
+    String input;
 
     public void startProgram() throws InterruptedException {
 
-        System.out.println("Welcome to STI Bank!");
-        System.out.println("Please insert your pin.");
-
-        if (acc.verifyPin(sc.nextLine())) {
-            System.out.println("Your account has been successfully verified.");
-
-        } else {
-            System.out.println("Wrong pin. Try again.");
-        }
-        // Implement fix when invalid input, return a statement rather than crashing
         while (running) {
-            System.out.println("You can check your balance, deposit, withdraw or exit.");
+            System.out.println("Welcome to STI Bank!");
+            System.out.println("Please insert your pin or exit the program.");
 
-            String input = sc.nextLine();
-            switch(input) {
-                case "balance", "Balance":
-                    System.out.println("Your total balance is: " + balance.getBalance());
-                    break;
+            input = sc.nextLine();
+            if (acc.verifyPin(input)) {
+                System.out.println("Your account has been successfully verified.");
+                loggedIn = true;
 
-                case "deposit", "Deposit":
-                    System.out.println("How much would you like to deposit?");
-                    balance.deposit(sc.nextDouble());
-                    sc.nextLine();
-                    Thread.sleep(1000);
-                    System.out.println("Your total balance is now: " + balance.getBalance() + " kr");
-                    Thread.sleep(1000);
-                    break;
+            } else if (input.equalsIgnoreCase("exit")) {
+                System.out.println("Shutting down. Thank you for using STI Bank!");
+                running = false;
+            } else {
+                System.out.println("Wrong pin. Try again.");
+            }
+            // Implement fix when invalid input, return a statement rather than crashing
+            while (loggedIn) {
+                System.out.println("You can check your balance, deposit, withdraw or logout.");
 
-                case "withdraw", "Withdraw":
-                    System.out.println("How much would you like to withdraw?");
-                    balance.withdraw(sc.nextDouble());
-                    sc.nextLine();
-                    Thread.sleep(1000);
-                    System.out.println("You have: " + balance.getBalance() + " kr left.");
-                    Thread.sleep(1000);
-                    break;
+                String input = sc.nextLine();
+                switch(input) {
+                    case "balance", "Balance":
+                        System.out.println("Your total balance is: " + balance.getBalance());
+                        break;
 
-                case "exit", "Exit":
-                    System.out.println("Thank you for using STI Bank!");
-                    running = false;
+                    case "deposit", "Deposit":
+                        System.out.println("How much would you like to deposit?");
+                        balance.deposit(sc.nextDouble());
+                        sc.nextLine();
+                        Thread.sleep(1000);
+                        System.out.println("Your total balance is now: " + balance.getBalance() + " kr");
+                        Thread.sleep(1000);
+                        break;
+
+                    case "withdraw", "Withdraw":
+                        System.out.println("How much would you like to withdraw?");
+                        balance.withdraw(sc.nextDouble());
+                        sc.nextLine();
+                        Thread.sleep(1000);
+                        System.out.println("You have: " + balance.getBalance() + " kr left.");
+                        Thread.sleep(1000);
+                        break;
+
+                    case "logout", "Logout":
+                        System.out.println("Logging out. Thank you for using STI Bank!");
+                        Thread.sleep(1000);
+                        loggedIn = false;
+                }
             }
         }
     }
